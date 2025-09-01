e70be21ac9471ecaa4d1e46d15676ee2ac113dc9
Ivan <volkivanv@mail.ru>
2025-09-02 01:23:25 +0300

    beforre bot

---
+++ cur/app/Telegram/TelegramApi.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Telegram;
+
+interface TelegramApi {
+    public function __construct(string $token);
+
+    /**
+     * @return TelegramMessageDto[]
+     */
+    public function getMessage(int $offset): array;
+    public function sendMessage(string $chatId, string $text);
+}
