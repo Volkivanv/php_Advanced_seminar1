cb5f9d8a9f27f86aa8ec9cc6bb37e0a1ba534092
Ivan <volkivanv@mail.ru>
2025-09-13 02:23:21 +0300

    tests added

---
+++ cur/tests/unit/HandleEventsDaemonCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class HandleEventsDaemonCommandTest extends TestCase
+{
+    /**
+     * @dataProvider getCurrentTimeDataProvider
+     */
+    public function testGetCurrentTime($expectedArray)
+    {
+        $handleEventsDaemonCommand = new \App\Commands\HandleEventsDaemonCommand(new \App\Application(dirname(__DIR__)));
+        $result = $handleEventsDaemonCommand->getCurrentTime();
+        $this->assertEquals($result, $expectedArray);
+    }
+
+    public static function getCurrentTimeDataProvider(): array
+    {
+        return [
+            [
+                [
+                date("i"),
+                date("H"),
+                date("d"),
+                date("m"),
+                date("w")
+                ]
+            ],
+        ];
+    }
+}
