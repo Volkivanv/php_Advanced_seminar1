76971d2c5d43fb8e0ede7272b8a38e2456664a7f
Ivan <volkivanv@mail.ru>
2025-09-23 02:33:46 +0300

    homework/seminar4

--- cur/README.md
+++ cur/README.md
@@ -4,7 +4,65 @@ docker run --rm -v ${pwd}/:/cli php:8.2-cli php /cli/vendor/phpunit/phpunit/phpu
 
 ### Содержимое crontab
 
-* * * * * /home/ivan/php_Advanced/lesson1/seminar1/cur/echo_date.sh
+* * * * * /home/ivan/php_Advanced/lesson1/seminar1/cur/echo_date
+
+## REDIS
+
+```bash
+127.0.0.1:6379> PFADD hll_key2 element21 element22 element23
+(integer) 1
+127.0.0.1:6379> PFADD hll_key1 element11 element12 element13
+(integer) 1
+127.0.0.1:6379> PFCOUNT hll_key1
+(integer) 3
+127.0.0.1:6379> PFCOUNT hll_key2
+(integer) 3
+127.0.0.1:6379> PFMERGE hll_key_merge2 hll_key1 hll_key2
+OK
+127.0.0.1:6379> PFCOUNT hll_key_merge2
+(integer) 6
+127.0.0.1:6379> SET my_key "Hello, Redis"
+OK
+127.0.0.1:6379> GET my_key
+"Hello, Redis"
+127.0.0.1:6379> 
+127.0.0.1:6379> SET my_key "Hello, Redis"
+OK
+127.0.0.1:6379> EXPIRE my_key 10
+(integer) 1
+127.0.0.1:6379> EXISTS my_key
+(integer) 1
+127.0.0.1:6379> EXISTS my_key
+(integer) 1
+127.0.0.1:6379> EXISTS my_key
+(integer) 0
+127.0.0.1:6379> 
+127.0.0.1:6379> SET counter 100
+OK
+127.0.0.1:6379> INCR counter
+(integer) 101
+127.0.0.1:6379> DECR counter
+
+ ┏━[02:25:22]  
+ ┃ /home/ivan/php_Advanced/lesson1/seminar1/cur
+ ┗━➤  php runner -c tg_messages
+[]]]]]%                                                                                              ┏━[02:26:03]  
+ ┃ /home/ivan/php_Advanced/lesson1/seminar1/cur
+ ┗━➤  redis-cli
+127.0.0.1:6379> KEYS *
+ 1) "hll_key"
+ 2) "tg_messages:offset"
+ 3) "hll_key2"
+ 4) "my_hash"
+ 5) "hll_key1"
+ 6) "counter"
+ 7) "my_bit"
+ 8) "my_set2"
+ 9) "hll_key_merge2"
+10) "my_set1"
+11) "tg_messages:old_messages"
+127.0.0.1:6379> 
 
 
-закончил 1:30:37
+```
+
--- cur/app/Commands/TgMessagesCommand.php
+++ cur/app/Commands/TgMessagesCommand.php
@@ -3,24 +3,85 @@
 namespace App\Commands;
 
 use App\Application;
+use App\Cache\Redis;
 use App\Telegram\TelegramApiImpl;   
+use Predis\Client;
 
 
 
 class TgMessagesCommand extends Command
 {
-    public function __construct(public Application $app)
+
+    protected Application $app;
+    private int $offset;
+    private array|null $oldMessages;
+
+    private Redis $redis;
+
+    public function __construct(public Application $inApp)
     {
-        $this->app = $app;
+        $this->app = $inApp;
+        $this->offset = 0;
+        $this->oldMessages = [];
+
+        $client = new Client([
+            'schema' => 'tcp',
+            'host' => '127.0.0.1',
+            'port' => 6379,
+        ]);
+
+        $this->redis = new Redis($client);
     }
 
     function run(array $options = []): void
     {
         $tgApi = new TelegramApiImpl($this->app->env('TELEGRAM_TOKEN'));
-        echo json_encode($tgApi->getMessage(0));
+      //  echo json_encode($tgApi->getMessage(0));
+        echo json_encode($this->receiveNewMessages());
     }
 
-    protected function getTelegramApi(array $options = [])
+    // public function handle(): void
+    // {
+    //     $messages = $this->receiveNewMessages();
+
+    //     foreach ($messages as $userId => $userMessages)
+    //     {
+    //         $answerMessage = $this->handleMessagesAndReturnAnswer($userMessages);
+
+    //         $this->eventSender->sendMessage($userId, $answerMessage);
+    //     }
+    // }
+
+    private function receiveNewMessages(): array
+    {
+        $this->offset = $this->redis->get('tg_messages:offset', 0);
+
+        $result = $this->getTelegramApi()->getMessage($this->offset);
+
+        $this->redis->set('tg_messages:offset', $result['offset'] ?? 0);
+
+        $this->oldMessages = json_decode($this->redis->get('tg_messages:old_messages'));
+
+        $messages = [];
+
+        foreach ($result['result'] ?? [] as $chatId => $newMessage)
+        {
+            if (isset($this->oldMessages[$chatId])){
+
+                $this->oldMessages[$chatId] = [...$this->oldMessages[$chatId], ...$newMessage];
+
+            } else {
+                $this->oldMessages[$chatId] = $newMessage;
+            }
+        }
+
+        $this->redis->set('tg_messages:old_messages', json_encode($this->oldMessages));
+
+        return $messages;
+
+    }
+
+    protected function getTelegramApi()
     {
         return new TelegramApiImpl($this->app->env('TELEGRAM_TOKEN'));
     }
